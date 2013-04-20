579c06141cea0540b73735feb636f9f1405c54b0
Aaron Morais <aaron.m@live.ca>
2013-04-19 22:13:49 -0400

    AaronM - Draw Hello World on the canvas.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-window.alert("HELLO WORLD!");
+$(document).ready(function() {
+	draw();
+	startIntialStory();
+});
+
+function draw() {
+	canvas = document.getElementById('canvas');
+	ctx = canvas.getContext('2d');
+	ctx.fillStyle = "orange";
+	ctx.fillRect(0,0, canvas.width, canvas.height);
+	ctx.font="30px Arial";
+	ctx.strokeText("Hello World",10,50);
+}
