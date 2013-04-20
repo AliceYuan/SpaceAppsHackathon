46dad288a5e76f378ee4885a8b85c44830f04d02
Aaron Morais <aaron.m@live.ca>
2013-04-20 11:33:47 -0400

    AaronM - Add GameEventManager and a data model json file

---
+++ js/GameEventManager.js
@@ -0,0 +1,53 @@
+function GameEventManager() {
+	this.events = [];
+	this.currentEvent;
+
+	this.filterValidEvents = function() {
+
+	};
+
+	this.validateEvent = function(event) {
+
+	};
+
+	this.getNextEvent = function (choice) {
+		this.getAllEvents();
+		this.turn +=1;
+		if(this.currentEvent == null) {
+			this.currentEvent = this.events[0];
+		} else {
+			this.currentEvent = this.getEventWithID(this.currentEvent.next[choice])
+		}
+		return this.currentEvent;
+	};
+
+	this.getEventWithID = function (ID) {
+		for (var i = 0; i < this.events.length; i++) {
+			var event = this.events[i];
+			if(event.ID == ID) {
+				return event;
+			} 
+		}
+		return null;
+	}
+
+	this.getAllEvents = function () {
+		eventJson = [];
+		$.ajax({
+		  url: 'data/game.json',
+		  async: false,
+		  dataType: 'json',
+		  success: function (response) {
+		  	eventJson = response;
+		  }
+		});
+		this.events = eventJson;
+		console.log(this.events);
+	};
+
+	this.executeNextEvent = function (choice) {
+		event = this.getNextEvent(choice);
+		console.log(event);
+		return event;
+	};
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-	draw();
-	startIntialStory();
+	// startIntialStory();
+	manager = new GameEventManager()
+	event = manager.executeNextEvent();
+	while(event != null) {
+		draw(event.description);
+		window.alert(event.title);
+		var y=window.prompt("please choose next")
+		event = manager.executeNextEvent(y);
+	}
 });
 
-function draw() {
+function draw(text) {
 	canvas = document.getElementById('canvas');
 	ctx = canvas.getContext('2d');
 	ctx.fillStyle = "orange";
 	ctx.fillRect(0,0, canvas.width, canvas.height);
 	ctx.font="30px Arial";
-	ctx.strokeText("Hello World",10,50);
+	ctx.strokeText(text,10,50);
 }
