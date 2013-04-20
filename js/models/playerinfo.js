c6b86a47cca42cbeb9b08bc817a2edb80b61b4bc
Jonathan Lo <jonathandlo@live.com>
2013-04-20 12:16:19 -0400

    added worldinfo.js model fixed typo in playerinfo.js

--- js/models/playerinfo.js
+++ js/models/playerinfo.js
@@ -11,8 +11,9 @@ function resources () {
 }
 
 function commodity () {
-	this.infrastructure = 0;
-	this.health = 100;
-	this.resources = 0;
-	this.money = 0;
+	this.helium = 0;
+	this.water = 0;
+	this.oxygen = 0;
+	this.hydrogen = 0;
+	this.preciousmetals = 0;
 }
