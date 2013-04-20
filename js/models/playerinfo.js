cf2c770827f3e8746de24641fcf811094e985dbc
Jonathan Lo <jonathandlo@live.com>
2013-04-20 12:54:55 -0400

    updated playerinfo.js removed worldinfo.js model

--- js/models/playerinfo.js
+++ js/models/playerinfo.js
@@ -1,13 +1,24 @@
 function playerinfo () {
-	this.resources = new resource();
+	this.resourcevalue = new resourcevalue();
+	this.resourcemax = new resourcemax();
 	this.commodity = new commodity();
+	this.infrastructure = new infrastructure();
+
+	this.healthpercent = function () { return playerinfo.resourcevalue / playerinfo.resourcemax; };
+	this.resourcepercent = 0;
+	this.moneypercent = 0;
 }
 
-function resources () {
-	this.power = 0;
-	this.health = 100;
+function resourcevalue () {
+	this.health = 50;
 	this.resources = 0;
 	this.money = 0;
+}
+
+function resourcemax () {
+	this.health = 100;
+	this.resources = 100;
+	this.money = 1000;
 }
 
 function commodity () {
@@ -16,4 +27,11 @@ function commodity () {
 	this.oxygen = 0;
 	this.hydrogen = 0;
 	this.preciousmetals = 0;
+}
+function infrastructure () {
+	this.rovers = 0;
+	this.solarpanels = 0;
+	this.printers = 0;
+	this.resourceextractors = 0;
+	this.waterextractors = 0;
 }
